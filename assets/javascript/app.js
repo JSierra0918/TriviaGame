08718a248b4506e6a7c328af05091f7d7253f379
Jorge <3d.jorgesierra@gmail.com>
2019-03-07 11:59:55 -0500

    working code with multiple questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,4 @@
-//grab a question
-var totalQuestions = 10;
-//display the question
-//grab answers
-//3 being wrong and 1 being correct
-// if it's right, then alert right!
-// if it's wrong, alert wrong
-
-// var questions = {
-//     q1 : "Where is the heart of a shrimp located?",
-//     q2 : "How many noses do slugs have?",
-//     q3 : "How long does it take a sloth to digest its food?"
-// };
-
+//Question objects
 
 var question1 = {
     question: "Where is the heart of a shirimp located at?",
@@ -27,24 +14,31 @@ var question2 = {
 
 var question3 = {
     question: "How long does it take a sloth to digest its food?",
-    answer: ["Two weeks", "1 week", "1 month", "16 hours"],
-    correct: "Two weeks"
+    answer: ["2 weeks", "1 week", "1 month", "16 hours"],
+    correct: "2 weeks"
 };
 
+//initialize variables
 
-var questionsArray = [question1,question2,question3];
+var questionsArray = [question1, question2, question3];
+var questionArrayRandom;
+var totalQuestions = 10;
+var questionsRight = 0;
+var questionsWrong = 0;
 
 for (var i = 0; i < totalQuestions.length; i++) {
 
 }
 
-function GenerateRandomQuestion(obj) {
-    //generate random question
-    var randomObjectKey = Object.keys(obj);
-    return randomObjectKey = Math.floor(Math.random() * randomObjectKey.length);
+function reset (){
+    var totalQuestions = 10;
+    questionsArray = [question1, question2, question3];
 
 }
 
+function timer (){
+
+}
 
 function generateQuestion() {
     //generate random question
@@ -52,24 +46,24 @@ function generateQuestion() {
 
     //select a random question
     var questionArrayRandomNum = Math.floor(Math.random() * questionsArray.length);
-    var questionArrayRandom = questionsArray[questionArrayRandomNum];
+    questionArrayRandom = questionsArray[questionArrayRandomNum];
     console.log(questionArrayRandom)
 
     // get the ANSWER array from the question
     var questionsLength = questionArrayRandom.answer.length;
-   
+
     //populate DOM with Text
     questionTitle.text(questionArrayRandom.question);
- 
+
+    //generate random answers
     for (var i = 0; i < questionsLength; i++) {
-        
+
         console.log(questionArrayRandom.answer);
         //reinitiliaze on every loop
         var answerElement = $("<div>");
         var randomIndex = Math.floor(Math.random() * questionArrayRandom.answer.length);
 
-        console.log(questionArrayRandom.answer[i]);
-
+        //assign class and text to new div
         answerElement
             .addClass("answer")
             .text(questionArrayRandom.answer[randomIndex]);
@@ -80,10 +74,41 @@ function generateQuestion() {
         questionArrayRandom.answer.splice(randomIndex, 1);
     }
 
+    //remove question so it does not repeat
+    questionsArray.splice(questionArrayRandomNum, 1);
+    console.log(questionArrayRandomNum);
+
+}
+
+function nextQuestion () {
+    //removes current answer class
+    $(".answer").remove();
+    generateQuestion();
+    onClick();
+}
+
+//Click event with winning condition
+function onClick (){
+    $(".answer").on("click", function () {
+
+        if ($(this).html() === questionArrayRandom.correct) {
+            console.log("win!");
+            questionsRight++
+            nextQuestion();
+        }else{
+            console.log("Wrong!");
+            questionsWrong++;
+            nextQuestion();
+        }
+    
+        console.log("click");
+    });
 }
 
 
-//$(this).html() === questionArray.correct;
+//Generate Question
 generateQuestion();
+onClick();
+
 
 
